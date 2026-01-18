0665472f64cbacdc00c620c8c1069d70678dc987
charansaigompa <gompacharansai10@gmail.com>
2026-01-18 16:37:25 +0530

    rendering shimmer card in a map

--- src/components/ShimmerUI.jsx
+++ src/components/ShimmerUI.jsx
@@ -4,21 +4,9 @@ import ShimmerCard from './ShimmerCard'
 const ShimmerUI = () => {
   return (
     
-    <div className=' flex flex-wrap '>
-      <ShimmerCard/>
-      <ShimmerCard/>
-      <ShimmerCard/>
-      <ShimmerCard/>
-      <ShimmerCard/>
-      <ShimmerCard/>
-      <ShimmerCard/>
-      <ShimmerCard/>
-      <ShimmerCard/>
-      <ShimmerCard/>
-      <ShimmerCard/>
-      <ShimmerCard/>
-      <ShimmerCard/>
-      <ShimmerCard/>
+    <div className=' flex flex-wrap justify-center '>
+   
+      {Array(14).fill(0).map((_,index)=>( <ShimmerCard key={index} />))}
     </div>
    
   )
