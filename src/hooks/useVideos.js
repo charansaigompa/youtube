924e60ef5e5c0742328fcf0c757f1c7c2bdc1777
charansaigompa <gompacharansai10@gmail.com>
2026-01-18 16:29:12 +0530

    optimized api call

--- src/hooks/useVideos.js
+++ src/hooks/useVideos.js
@@ -3,30 +3,19 @@ import { useDispatch, useSelector } from "react-redux";
 import { YOUTUBE_API } from "../utils/constants";
 import { addMostPopularVideos } from "../utils/videosSearch";
 
-
-
-
-
-export const useVideos=()=>{
- const videos=useSelector(store=>store.videos.mostPopularVideos)
-    const dispatch=useDispatch();
-
-
-  const getVideos=async()=>{
-    const data=await fetch(YOUTUBE_API);
-    const json=await data.json()
-   
-    
-  dispatch(addMostPopularVideos(json.items))
-   // console.log(json)
-   // console.log(json.items)
-  }
-   useEffect(()=>{
-    if(!videos){
-       getVideos();
+export const useVideos = () => {
+  const videos = useSelector((store) => store.videos.mostPopularVideos);
+  const dispatch = useDispatch();
+  const getVideos = async () => {
+    const data = await fetch(YOUTUBE_API);
+    const json = await data.json();
+    dispatch(addMostPopularVideos(json.items));
+  };
+  useEffect(() => {
+    if (!videos) {
+      getVideos();
     }
-   
-  },[videos])
-   
+  }, [videos]);
+
   return videos;
-}
+};
